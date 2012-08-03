1398237be846689ed1221bd2459e9bd8001a4eec
James Lin <jameslin101@gmail.com>
2012-08-03 03:16:23 -0400

    fixed error messages

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,9 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, :through => :recipe_ingredients, :uniq => true
 
+  validates :name, :presence => true
+  validates_uniqueness_of :name, :case_sensitive => false
+
   accepts_nested_attributes_for :recipe_ingredients, 
                                 :allow_destroy => true,
                                 :reject_if => lambda { |a| a[:quantity].blank? ||
